5763e4cf032b72678664217c861bb39560381e8a
Dominic LoBue <dominic.lobue@gmail.com>
2011-08-11 16:17:04 -0700

    bump version number, remove unnecessary deps.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.2'
+version = '0.3.0'
 
 setup(name='powerupdater',
       version=version,
-      description="Update powerdns sqlite db with new info from AWS",
-      long_description="""\
-""",
+      description="Update route53 dns zone with new info from AWS",
+      #long_description="""\
+#""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Dominic LoBue',
@@ -19,9 +19,7 @@ setup(name='powerupdater',
       zip_safe=False,
       scripts = ['bin/powerupdater'],
       install_requires=[
-          'boto>=2.0b3',
-          'sqlobject>0.12.2',
-          # -*- Extra requirements: -*-
+          'boto>=2.0rc1',
       ],
       entry_points="""
       # -*- Entry points: -*-
